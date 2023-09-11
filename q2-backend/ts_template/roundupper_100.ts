f68a25613ca461c35d485778eeba99636f572056
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-09-11 18:50:37 +1000

    Finish part1

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -1,16 +1,16 @@
-import express from "express";
+import express from 'express';
 
 // location is the simple (x, y) coordinates of an entity within the system
 // spaceCowboy models a cowboy in our super amazing system
 // spaceAnimal models a single animal in our amazing system
 type location = { x: number; y: number };
 type spaceCowboy = { name: string; lassoLength: number };
-type spaceAnimal = { type: "pig" | "cow" | "flying_burger" };
+type spaceAnimal = { type: 'pig' | 'cow' | 'flying_burger' };
 
 // spaceEntity models an entity in the super amazing (ROUND UPPER 100) system
 type spaceEntity =
-  | { type: "space_cowboy"; metadata: spaceCowboy; location: location }
-  | { type: "space_animal"; metadata: spaceAnimal; location: location };
+  | { type: 'space_cowboy'; metadata: spaceCowboy; location: location }
+  | { type: 'space_animal'; metadata: spaceAnimal; location: location };
 
 // === ADD YOUR CODE BELOW :D ===
 
@@ -18,13 +18,21 @@ type spaceEntity =
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
 
+app.use(express.json());
+
 // the POST /entity endpoint adds an entity to your global space database
-app.post("/entity", (req, res) => {
-  // TODO: fill me in
+app.post('/entity', (req, res) => {
+  const { entities } = req.body;
+
+  entities.forEach((entity: spaceEntity) => {
+    spaceDatabase.push(entity);
+  });
+
+  res.status(200).send();
 });
 
 // /lassoable returns all the space animals a space cowboy can lasso given their name
-app.get("/lassoable", (req, res) => {
+app.get('/lassoable', (req, res) => {
   // TODO: fill me in
 });
 
